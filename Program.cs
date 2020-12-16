6dea345dd61e88e6d02886d0f1f7f458d3a8745c
Deborah Novais <61918962+deborahnovais@users.noreply.github.com>
2020-12-15 21:16:53 -0300

    Concluído

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Array");
+
+            string[] nomes = new string[3];
+            int[] idades = new int[3];
+
+            for (var contador = 0; contador < 3; contador++) {
+                Console.WriteLine($"Digite o {contador+1}º nome");
+                nomes[contador] = Console.Readline();
+
+                Console.WriteLine("Digite a idade");
+                idades[contador] = int.Parse(Console.Readline());
+            }
+
+            Console.WriteLine("Nomes cadastrados:");
+            
+            for (var contador = 0; contador < 3; contador++) {
+                Console.WriteLine("Nome: " +nomes[contador]);
+                Console.WriteLine("Idade: " +idades[contador]);
+            }
+
+        }
+    }
+}
